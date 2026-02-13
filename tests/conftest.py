fdd6eb1615672b7a454ca10c4078a876afce067b
vidhyamoorthy2011-bit <vidhyamoorthy2011@gmail.com>
2026-02-13 17:04:50 +0300

    Use headless Chrome for Selenium in Jenkins pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,9 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def setup():
     options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
     options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
