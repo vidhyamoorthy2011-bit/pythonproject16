0dd2f1dba40060cb4c48ff60581d8dc091281513
vidhyamoorthy2011-bit <vidhyamoorthy2011@gmail.com>
2026-02-14 10:11:29 +0300

    Fix Edge headless crash in Jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,10 +7,11 @@ from selenium.webdriver.edge.options import Options
 @pytest.fixture
 def setup():
     options = Options()
-    options.add_argument("--headless=True")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--start-maximized")
+    options.add_argument("--window-size=1920,1080")
     driver = webdriver.Edge(options=options)
 
     yield driver
