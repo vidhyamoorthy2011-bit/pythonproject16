ce6c82a8a946a6101897a2b515909dfbf7e21c7f
vidhyamoorthy2011-bit <vidhyamoorthy2011@gmail.com>
2026-02-15 09:02:47 +0300

    Fix Edge crash in Jenkins using headless CI-safe options

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,12 +11,19 @@ def setup():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
     options.add_argument("--disable-software-rasterizer")
     options.add_argument("--remote-debugging-port=9222")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("--no-first-run")
+    options.add_argument("--no-default-browser-check")
+
+    options.binary_location = r"C:\Program Files\Microsoft\Edge\Application\msedge.exe"
+
     service = Service()
 
     driver = webdriver.Edge(service=service, options=options)
+    driver.implicitly_wait(10)
 
     yield driver
     driver.quit()
