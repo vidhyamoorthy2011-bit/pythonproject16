cea0bdd64c1fd0cdad83beb85a0c0a74531b9af9
vidhyamoorthy2011-bit <vidhyamoorthy2011@gmail.com>
2026-02-15 09:40:22 +0300

    first comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,16 +7,11 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def setup():
     options = Options()
+    options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
     options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--remote-debugging-port=9222")
     options.add_argument("--window-size=1920,1080")
 
-
-    options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
 
     service = Service()
 
