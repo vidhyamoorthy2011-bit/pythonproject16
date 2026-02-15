a1a3936e96e35afab598f6a9a6edbbebca620057
vidhyamoorthy2011-bit <vidhyamoorthy2011@gmail.com>
2026-02-15 09:25:20 +0300

    Fix chrome installed and update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
@@ -12,17 +12,15 @@ def setup():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-extensions")
-    options.add_argument("--disable-software-rasterizer")
     options.add_argument("--remote-debugging-port=9222")
     options.add_argument("--window-size=1920,1080")
-    options.add_argument("--no-first-run")
-    options.add_argument("--no-default-browser-check")
 
-    options.binary_location = r"C:\Program Files\Microsoft\Edge\Application\msedge.exe"
+
+    options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
 
     service = Service()
 
-    driver = webdriver.Edge(service=service, options=options)
+    driver = webdriver.Chrome(service=service, options=options)
     driver.implicitly_wait(10)
 
     yield driver
